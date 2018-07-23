50cfacfcba75faba2fe91ace99015423b3e39ad2
Vyacheslav Blinov <blinov.vyacheslav@gmail.com>
2018-07-23 19:02:19 +0300

    Minimal akka hello-world

---
+++ src/main/scala/hello/HelloActor.scala
@@ -0,0 +1,10 @@
+package hello
+
+import akka.actor.Actor
+
+class HelloActor extends Actor {
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
+  }
+}
---
+++ src/main/scala/hello/Main.scala
@@ -0,0 +1,12 @@
+package hello
+
+import akka.actor.{ActorSystem, Props}
+
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
+
